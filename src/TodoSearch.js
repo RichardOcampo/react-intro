7b0735b018ed41554d9ab273424bd48eaa4cd5ae
RichardOcampo <vegetogamercraft@gmail.com>
2023-10-03 19:08:37 -0600

    se agregaron varias funciones

---
+++ src/TodoSearch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TodoSearch.css';
+
+function TodoSearch() {
+  return (
+    <input
+      placeholder="¿Que tengo pendiente?"
+      className="TodoSearch"
+    />
+  );
+}
+
+export { TodoSearch };
